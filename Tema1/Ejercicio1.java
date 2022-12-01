f8441289b230f85d2a0ecc766c0ac755f0a9a858
Jefferson <denis.cristal.230915@gmail.com>
2022-12-01 00:57:26 -0500

    Add files via upload

---
+++ Tema1/Ejercicio1.java
@@ -0,0 +1,25 @@
+package Tema1;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        String nombre = "Hola Mundo";
+        byte number = 100;       // 1 byte
+        short number2 = 10000;     // 2 byte
+        int number3 = 1000000000;       // 4 byte
+        long number4 = 1000000000;      // 8 byte
+        float decimal = 4.9f;
+        double decimal1 = 9.99f;
+        boolean verdadero = true;
+        boolean falso = false;
+
+        System.out.println("byte : " + number);
+        System.out.println("int : " + number3);
+        System.out.println("short : " + number2);
+        System.out.println("long : " + number4);
+        System.out.println("double : " + decimal1);
+        System.out.println("float : " + decimal);
+        System.out.println("String : " + nombre);
+        System.out.println("boolean : " + verdadero);
+        System.out.println("boolean : " + falso);
+    }
+}
