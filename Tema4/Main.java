1240c0b2219c56f3e9c9f652c6ff05f762c13257
Jefferson <denis.cristal.230915@gmail.com>
2022-12-02 00:49:39 -0500

    Update Main.java

--- Tema4/Main.java
+++ Tema4/Main.java
@@ -7,22 +7,11 @@ public class Main {
 
         SmartDevice smartDevice = new SmartDevice("Apple",2022);
         SmartWatch smartWatch = new SmartWatch("Apple",2022,"ZTE","Rojo","Reloj",583.21);
-
-        System.out.println(smartWatch.empresa);
-        System.out.println(smartWatch.modelo);
-        System.out.println(smartWatch.year);
-        System.out.println(smartWatch.color);
-        System.out.println(smartWatch.tipo);
-        System.out.println(smartWatch.precio);
-
         SmartPhone smartPhone = new SmartPhone("Apple",2022,"AMD","Negro","Celular",254.54);
+        
+        System.out.println(" Reloj " + smartWatch);
+        System.out.println(" Celular " + smartPhone);
 
-        System.out.println(smartPhone.empresa);
-        System.out.println(smartPhone.modelo);
-        System.out.println(smartPhone.year);
-        System.out.println(smartPhone.color);
-        System.out.println(smartPhone.tipo);
-        System.out.println(smartPhone.precio);
 
 
 
